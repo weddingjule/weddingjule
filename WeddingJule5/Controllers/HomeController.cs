9093e089722f6316a6c6ca046c3e572fedf32fdb
weddingjule5 <weddingjule5@mail.ru>
2015-05-19 10:05:14 +0300

    поддерживаем категории затрат

--- WeddingJule5/Controllers/HomeController.cs
+++ WeddingJule5/Controllers/HomeController.cs
@@ -19,5 +19,91 @@ namespace WeddingJule5.Controllers
             //var expenses = db.Expenses;
             return View(expenses.ToList());
         }
+
+        public ActionResult ListCategories(int? id)
+        {
+            if (id == null)
+            {
+                return HttpNotFound();
+            }
+
+            Category category = db.Categories.Find(id);
+            if (category == null)
+            {
+                return HttpNotFound();
+            }
+            category.expenses = db.Expenses.Where(m => m.CategoryId == category.Id);
+            return View(category);
+        }
+
+        [HttpGet]
+        public ActionResult Create()
+        {
+            SelectList categories = new SelectList(db.Categories, "Id", "name");
+            ViewBag.Categories = categories;
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Create(Expense expense)
+        {
+            db.Expenses.Add(expense);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet]
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return HttpNotFound();
+            }
+
+            Expense expense = db.Expenses.Find(id);
+            if (expense != null)
+            {
+                SelectList categories = new SelectList(db.Categories, "Id", "name");
+                ViewBag.Categories = categories;
+                return View(expense);
+            }
+
+            return RedirectToAction("Index");
+        }
+
+        [HttpPost]
+        public ActionResult Edit(Expense expense)
+        {
+            db.Entry(expense).State = EntityState.Modified;
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+
+        [HttpGet]
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return HttpNotFound();
+            }
+
+            Expense expense = db.Expenses.Find(id);
+            Category category = db.Categories.Find(expense.CategoryId);
+            expense.Category = category;
+
+            if (expense != null)
+                return View(expense);
+
+            return RedirectToAction("Index");
+        }
+
+        [HttpPost]
+        public ActionResult Delete(Expense expense)
+        {
+            db.Expenses.Remove(expense);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
     }
 }
--- WeddingJule5/Models/Category.cs
+++ WeddingJule5/Models/Category.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -8,6 +9,7 @@ namespace WeddingJule5.Models
     public class Category
     {
         public int Id { get; set; }
+        [Display(Name = "Категория траты")]
         public string name { get; set; }
         public IEnumerable<Expense> expenses { get; set; }
         //проверяем наличие чекина
