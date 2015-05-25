719ca06e2971db3d5e55117ef7a8f31a09a52ecd
weddingjule5 <weddingjule5@mail.ru>
2015-05-25 14:44:53 +0300

    под мобилку - теперь и хелпер категорий

---
+++ WeddingJule5/Helpers/HeaderHelpers.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WeddingJule.Helpers
+{
+    public enum HeaderVariant{ListExpense, ListCategory}
+
+    public static class HeaderHelpers
+    {
+        private static HeaderLinkInfo[] headerLinkInfos;
+        private static UrlHelper urlHelper = new UrlHelper(HttpContext.Current.Request.RequestContext);
+
+        static HeaderHelpers()
+        {
+            headerLinkInfos = new HeaderLinkInfo[2];
+            headerLinkInfos[0] = new HeaderLinkInfo(HeaderVariant.ListExpense, "Expense", new { PageNumber = 1, category = 0 }, "Список затрат", urlHelper);
+            headerLinkInfos[1] = new HeaderLinkInfo(HeaderVariant.ListCategory, "Category", null, "Список категорий", urlHelper);
+        }
+
+        public static MvcHtmlString HeaderLinks(this HtmlHelper html, HeaderVariant headerVariant)
+        {
+            StringBuilder result = new StringBuilder();
+
+            foreach (HeaderLinkInfo headerLinkInfo in headerLinkInfos)
+            {
+                TagBuilder tag = new TagBuilder("a");
+
+                tag.MergeAttribute("href", headerLinkInfo.urlString);
+                tag.InnerHtml = headerLinkInfo.linkCaption;
+
+                if (headerLinkInfo.headerVarinat == headerVariant)
+                {
+                    tag.AddCssClass("selected");
+                    tag.AddCssClass("btn-primary");
+                }
+                tag.AddCssClass("btn btn-default");
+                result.Append(tag.ToString());
+            }            
+
+            return MvcHtmlString.Create(result.ToString());
+        }
+    }
+
+    public class HeaderLinkInfo
+    {
+        public readonly HeaderVariant headerVarinat;
+        public readonly string urlString;
+        public readonly string Controller;
+        public readonly string linkCaption;
+
+        public HeaderLinkInfo(HeaderVariant headerVariant, string Controller, object routeValues, string linkCaption, UrlHelper urlHelper)
+        {
+            this.headerVarinat = headerVariant;
+            this.Controller = Controller;
+            this.urlString = urlHelper.Action(headerVariant.ToString(), Controller, routeValues);
+            this.linkCaption = linkCaption;
+        }
+    }
+}
