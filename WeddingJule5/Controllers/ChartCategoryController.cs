321ba49091fdeef41cbf996c3c6bee0a6b5bf406
weddingjule5 <weddingjule5@mail.ru>
2015-05-26 10:35:30 +0300

    распределение затрат по категориям готово

--- WeddingJule5/Controllers/ChartCategoryController.cs
+++ WeddingJule5/Controllers/ChartCategoryController.cs
@@ -7,6 +7,8 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using WeddingJule.Models;
+using System.Data.Entity;
+using System.Globalization;
 
 namespace WeddingJule.Controllers
 {
@@ -31,6 +33,55 @@ namespace WeddingJule.Controllers
             ChartCategory chartCategory = new ChartCategory(charDatas.OrderByDescending(c=>c.price).AsEnumerable<CharData>());
 
             return View("ChartCategory", chartCategory);
+        }
+
+        public ActionResult LineCategory()
+        {
+            List<Expense> allExpenses = db.Expenses.ToList<Expense>();
+            List<Category> categories = db.Categories.ToList<Category>();
+            
+            foreach(Category category in categories)
+            {
+                IEnumerable<Expense> categoryExpenses = allExpenses.Where(p => p.CategoryId == category.Id);
+
+                IEnumerable<CategoryWeekInfo> categoryWeekInfos = from p in categoryExpenses
+                                                                  group p by new { month = p.date.Month } into m
+                                                                  orderby m.Key.month
+                                                                  select new CategoryWeekInfo(m.Key.month, m.Sum(c => c.price), category.name);
+
+                CategoryWeekInfoViewModel cc = new CategoryWeekInfoViewModel(categoryWeekInfos);
+
+                return View(cc);
+            }
+
+
+
+            return View();
+        }
+    }
+
+    public class CategoryWeekInfoViewModel
+    {
+        public readonly IEnumerable<CategoryWeekInfo> categoryWeekInfos;
+        public CategoryWeekInfoViewModel(IEnumerable<CategoryWeekInfo> categoryWeekInfos)
+        {
+            this.categoryWeekInfos = categoryWeekInfos;
+        }
+    }
+
+    public class CategoryWeekInfo
+    {
+        public readonly int month;
+        public readonly string monthName;
+        public readonly decimal price;
+        public readonly string categoryName;
+
+        public CategoryWeekInfo(int month, decimal price, string categoryName)
+        {
+            this.month = month;
+            this.monthName = CultureInfo.CurrentCulture.DateTimeFormat.GetMonthName(month);
+            this.price = price;
+            this.categoryName = categoryName;
         }
     }
 
