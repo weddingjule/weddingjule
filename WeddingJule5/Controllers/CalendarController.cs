c1384ec3a4f8b077877dae325132daa38a521e42
Саша <weddingauth@mail.ru>
2015-06-26 10:46:50 +0300

    календарь с собственным тулптипом!

--- WeddingJule5/Controllers/CalendarController.cs
+++ WeddingJule5/Controllers/CalendarController.cs
@@ -19,12 +19,23 @@ namespace WeddingJule.Controllers
 
         public JsonResult GetData()
         {
-            IEnumerable<DayInfo> days = from expense in db.Expenses
+            IQueryable<Expense> expenses = db.Expenses.AsQueryable<Expense>();
+
+            IEnumerable<DayInfo> days = from expense in expenses
                                         group expense by expense.date into m
                                         orderby m.Key
-                         select new DayInfo () { date = m.Key, price = m.Sum(e=>e.price) };
+                                        select new DayInfo() { date = m.Key, price = m.Sum(e => e.price)};
 
-            return Json(days, JsonRequestBehavior.AllowGet);
+            List<DayInfo> dayList = days.ToList<DayInfo>();
+
+            foreach(DayInfo day in dayList)
+            {
+                IEnumerable<Expense> dayExpenses = expenses.Where<Expense>(e => e.date == day.date);
+                day.setInfo(dayExpenses);
+                string d = day.info;
+            }
+
+            return Json(dayList, JsonRequestBehavior.AllowGet);
         }
     }
 
@@ -32,5 +43,24 @@ namespace WeddingJule.Controllers
     {
         public DateTime date;
         public decimal price;
+
+        public void setInfo(IEnumerable<Expense> expenses)
+        {
+            string expenseNameInfo = string.Empty;
+
+            int count = expenses.Count();
+            if (count == 1)
+                expenseNameInfo = expenses.ElementAt<Expense>(0).name;
+            else if (count > 1)
+            {
+                decimal maxprice = expenses.Max(e => e.price);
+                Expense expense = expenses.Where(e => e.price == maxprice).Single();
+                expenseNameInfo = expense.name;
+            }
+
+            info = date.Date.ToShortDateString() + " на " + price.ToString() + ": " + expenseNameInfo;
+        }
+
+        public string info;
     }
 }
