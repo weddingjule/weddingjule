0401bf635fdbc4b955e00c89506f7b5a3d2afe02
Саша <weddingauth@mail.ru>
2015-07-31 13:15:46 +0300

    активность дня

---
+++ WeddingJule5/Controllers/DayActivityController.cs
@@ -0,0 +1,18 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WeddingJule.Controllers
+{
+    public class DayActivityController : Controller
+    {
+        [HttpPost]
+        public RedirectResult Index(JsonResult j)
+        {
+            return RedirectPermanent("/Angular/DayActivity.html");
+        }
+    }
+}
